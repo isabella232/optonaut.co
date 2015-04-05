fc73a39d700acbdfcec869f560cf28b6a5476632
Johannes Schickling <schickling.j@gmail.com>
2015-04-05 22:38:13 +0200

    home

--- app/components/HomeSection/index.jsx
+++ app/components/HomeSection/index.jsx
@@ -12,9 +12,17 @@ let HomeSection = React.createClass({
           <video autoPlay id='bgvid' loop poster={require('../../assets/images/home.jpg')}>
           </video>
         </div>
-        <div className='container'>
+        <div className='container' id='section-home-content'>
           <div id='section-home-headline'>
             <h1>Dive Into Your Pictures</h1>
+            <span>Take Virtual Reality Photographs With Your Smartphone.</span>
+          </div>
+          <div className='icon' id='section-home-play'></div>
+        </div>
+        <div id='section-home-foot'>
+          <div className='container' id='section-home-foot-wrapper'>
+            <div id='section-home-foot-desc'>The Optonaut App is <strong>invite only</strong> at this stage. Request an invite to get <strong>early access</strong>.</div>
+            <div id='section-home-foot-button'>Reqest Invite</div>
           </div>
         </div>
       </div>
