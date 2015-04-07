c22e6f56661e239cabe9d24b84d0c1ee75981256
Johannes Schickling <schickling.j@gmail.com>
2015-04-07 18:28:20 +0200

    crowdfunding

--- app/components/CampaignSection/index.jsx
+++ app/components/CampaignSection/index.jsx
@@ -9,7 +9,57 @@ export default class CampaignSection extends React.Component {
     return (
       <div id='section-campaign'>
         <div className='container' id='section-campaign-content'>
-          <h2>We Are Crowdfunding</h2>
+          <div className='row' id='section-campaign-head'>
+            <div className='one-half column'>
+              <h2>We Are Crowdfunding</h2>
+            </div>
+            <div className='one-half column'></div>
+          </div>
+          <div className='row'>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy <strong>text ever since the 1500s</strong>.</p>
+          </div>
+          <div className='row'>
+            <div className='one-half column'>
+            Zitat
+            </div>
+            <div className='one-half column'>
+              <div id='section-home-foot-button'>Yes, I Support This</div>
+            </div>
+          </div>
+          <div className='row' id='section-campaign-rewards'>
+            <div className='three columns'>
+            Reward 1
+            </div>
+            <div className='three columns'>
+            Reward 2
+            </div>
+            <div className='three columns'>
+            Reward 3
+            </div>
+            <div className='three columns'>
+            Reward 4
+            </div>
+          </div>
+          <div className='row'>
+            <div className='two columns'>
+              Press 1
+            </div>
+            <div className='two columns'>
+              Press 1
+            </div>
+            <div className='two columns'>
+              Press 1
+            </div>
+            <div className='two columns'>
+              Press 1
+            </div>
+            <div className='two columns'>
+              Press 1
+            </div>
+            <div className='two columns'>
+              Press 1
+            </div>
+          </div>
         </div>
       </div>
     );
