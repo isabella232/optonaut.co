eae7b32fbe6b62beeb9f691fcd104eac24f75df0
Johannes Schickling <schickling.j@gmail.com>
2015-04-08 22:42:39 +0200

    crowdfunding

--- app/components/CampaignSection/index.jsx
+++ app/components/CampaignSection/index.jsx
@@ -14,20 +14,35 @@ export default class CampaignSection extends React.Component {
           </div>
           <div id='section-campaign-cite'>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy <strong>text ever since the 1500s</strong>.</p>
-            <span id='section-campaign-author'>Johannes Schickling, Co-Founder<img src={require('../../assets/images/face.jpg')} id='section-campaign-head' /></span>
+            <span id='section-campaign-author'>Johannes Schickling, Co-Founder<img id='section-campaign-head' src={require('../../assets/images/face.jpg')} /></span>
           </div>
           <div id='section-campaign-button'>
             <a className='button' href="#">Yes, I Want To Support Optonaut</a>
           </div>
           <div className='row' id='section-campaign-rewards'>
             <div className='one-third column'>
-            Reward 1
+              <div className='section-campaign-reward-price'>5$</div>
+              <div className='section-campaign-reward-info'>
+                <div className='section-campaign-reward-title'>VR Glasses</div>
+                <div className='section-campaign-reward-status'>3453/5000 left</div>
+              </div>
+              <div className='section-campaign-reward-image'><img src={require('../../assets/images/cardboard.png')} /></div>
             </div>
             <div className='one-third column'>
-            Reward 2
+              <div className='section-campaign-reward-price'>5$</div>
+              <div className='section-campaign-reward-info'>
+                <div className='section-campaign-reward-title'>VR Glasses</div>
+                <div className='section-campaign-reward-status'>3453/5000 left</div>
+              </div>
+              <div className='section-campaign-reward-image'><img src={require('../../assets/images/hardware2.png')} /></div>
             </div>
             <div className='one-third column'>
-            Reward 3
+              <div className='section-campaign-reward-price'>5$</div>
+              <div className='section-campaign-reward-info'>
+                <div className='section-campaign-reward-title'>VR Glasses</div>
+                <div className='section-campaign-reward-status'>3453/5000 left</div>
+              </div>
+              <div className='section-campaign-reward-image'><img src={require('../../assets/images/hardware3.png')} /></div>
             </div>
           </div>
         </div>
