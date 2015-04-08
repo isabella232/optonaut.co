259b6aacdae4ca1f549dd83e3db3b0d0df9ea179
Johannes Schickling <schickling.j@gmail.com>
2015-04-08 22:07:06 +0200

    face

--- app/components/CampaignSection/index.jsx
+++ app/components/CampaignSection/index.jsx
@@ -9,28 +9,25 @@ export default class CampaignSection extends React.Component {
     return (
       <div id='section-campaign'>
         <div className='container' id='section-campaign-content'>
-          <div className='row' id='section-campaign-head'>
-            <div className='one-half column'>
-              <h2>We Are Crowdfunding</h2>
-            </div>
-            <div className='one-half column'></div>
+          <div id='section-campaign-headline'>
+            <h2>We Are Crowdfunding</h2>
           </div>
-          <div className='row'>
+          <div id='section-campaign-cite'>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy <strong>text ever since the 1500s</strong>.</p>
+            <span id='section-campaign-author'>Johannes Schickling, Co-Founder<img src={require('../../assets/images/face.jpg')} id='section-campaign-head' /></span>
           </div>
-          <a href="#" className='button'>Yes, I Support This</a>
+          <div id='section-campaign-button'>
+            <a className='button' href="#">Yes, I Want To Support Optonaut</a>
+          </div>
           <div className='row' id='section-campaign-rewards'>
-            <div className='three columns'>
+            <div className='one-third column'>
             Reward 1
             </div>
-            <div className='three columns'>
+            <div className='one-third column'>
             Reward 2
             </div>
-            <div className='three columns'>
+            <div className='one-third column'>
             Reward 3
-            </div>
-            <div className='three columns'>
-            Reward 4
             </div>
           </div>
         </div>
