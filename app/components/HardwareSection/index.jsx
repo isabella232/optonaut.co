b1c8886c316b8e50b87a966c287522a73e011cce
Johannes Schickling <schickling.j@gmail.com>
2015-04-08 17:40:21 +0200

    images

--- app/components/HardwareSection/index.jsx
+++ app/components/HardwareSection/index.jsx
@@ -13,15 +13,15 @@ export default class HardwareSection extends React.Component {
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy <strong>text ever since the 1500s,</strong> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five.</p>
           <div className='row' id='section-hardware-list'>
             <div className='one-third column'>
-              <div className='icon' id='section-hardware-feature1'></div>
+              <img src={require('../../assets/images/hardware1.png')} />
               <span>Easy Capturing Process</span>
             </div>
             <div className='one-third column'>
-              <div className='icon' id='section-hardware-feature2'></div>
+              <img src={require('../../assets/images/hardware2.png')} />
               <span>Full 360° In Real 3D</span>
             </div>
             <div className='one-third column'>
-              <div className='icon' id='section-hardware-feature3'></div>
+              <img src={require('../../assets/images/hardware3.png')} />
               <span>View With Any VR Glasses</span>
             </div>
           </div>
