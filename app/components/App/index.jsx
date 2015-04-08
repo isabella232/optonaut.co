0d9159980976475ee0c290b359613cca7cfcd765
Johannes Schickling <schickling.j@gmail.com>
2015-04-08 18:40:07 +0200

    contact

--- app/components/App/index.jsx
+++ app/components/App/index.jsx
@@ -1,13 +1,14 @@
 'use strict';
 
 import React from 'react';
+import NavBar from '../NavBar';
 import HomeSection from '../HomeSection';
 import CampaignSection from '../CampaignSection';
 import FeaturesSection from '../FeaturesSection';
 import HardwareSection from '../HardwareSection';
 import FaqSection from '../FaqSection';
+import ContactSection from '../ContactSection';
 import Footer from '../Footer';
-import NavBar from '../NavBar';
 
 require('./index.less');
 
@@ -21,6 +22,7 @@ export default class App extends React.Component {
         <FeaturesSection/>
         <HardwareSection/>
         <FaqSection/>
+        <ContactSection/>
         <Footer/>
       </div>
     );
---
+++ app/components/ContactSection/index.jsx
@@ -0,0 +1,62 @@
+'use strict';
+
+import React from 'react';
+
+require('./index.less');
+
+function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
+
+export default class ContactSection extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      invalidInput: false,
+      submitted: false
+    };
+  }
+
+  submit() {
+    const email = React.findDOMNode(this.refs.email).value;
+    if (!email || validateEmail(email)) {
+      this.setState({ submitted: true });
+    } else {
+      this.setState({ invalidInput: true });
+      setTimeout(function() {
+        this.setState({ invalidInput: false });
+      }.bind(this), 500);
+    }
+  }
+
+  watchForEnter(e) {
+    if (e.keyCode === 13) {
+      this.submit();
+    }
+  }
+
+  render() {
+    return (
+      <div id='section-contact'>
+        <div className='container' id='section-contact-content'>
+          <h2>Let Us Know What You Think</h2>
+          <div id='section-contact-form'>
+            <div className='row'>
+              <textarea placeholder='Do you have any questions?'></textarea>
+            </div>
+            <div className='row'>
+              <div className='one-half column'>
+                <input className={this.state.invalidInput ? 'invalid' : ''} name='email' onKeyUp={this.watchForEnter.bind(this)} placeholder='Email (optional)' ref='email' type='email' />
+              </div>
+              <div className='one-half column'>
+                <div className='button' onClick={this.submit.bind(this)}>Send Message</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
