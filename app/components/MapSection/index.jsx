d5204de811eae1736d90122ac33f5f4f77f7d623
Johannes Schickling <schickling.j@gmail.com>
2015-04-09 22:36:39 +0200

    image swtich

--- app/components/MapSection/index.jsx
+++ app/components/MapSection/index.jsx
@@ -1,26 +1,42 @@
 'use strict';
 
-import React from 'react';
+import React from 'react/addons';
+import _ from 'lodash';
 
 require('./index.less');
 
+const numPoints = 7;
+let interval;
+
 export default class MapSection extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePoint: 0
+    };
+
+    interval = setInterval(function() {
+      const activePoint = (this.state.activePoint + 1) % numPoints;
+      this.setState({ activePoint });
+    }.bind(this), 4300);
+  }
+
+  selectPoint(i) {
+    this.setState({ activePoint: i });
+    clearInterval(interval);
+  }
+
   render() {
+    const activePoint = this.state.activePoint;
+    const pois = _.range(numPoints).map(i => <div className={i === activePoint ? 'active' : ''} onClick={this.selectPoint.bind(this, i)}></div>);
     return (
       <div id='section-map'>
         <div id='section-map-wrapper'>
           <div className='container' id='section-map-content'>
-            <div id='section-map-phone'><img src={require('../../assets/images/app.jpg')} /></div>
+            <div id='section-map-phone'><div><img key={activePoint} src={require(`../../assets/images/app${activePoint}.jpg`)} /></div></div>
             <div id='section-map-pois'>
-              <div id='section-map-pois-wrapper'>
-                <div className='active'></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
+              <div id='section-map-pois-wrapper'>{pois}</div>
             </div>
           </div>
         </div>
