b82772663b9fe5a66289a919e0851eb79227a064
Johannes Schickling <schickling.j@gmail.com>
2015-04-11 19:57:23 +0200

    gears

--- app/components/ProcessSection/index.jsx
+++ app/components/ProcessSection/index.jsx
@@ -4,7 +4,29 @@ import React from 'react';
 
 require('./index.less');
 
+const numSteps = 3;
+let interval;
+
 export default class ProcessSection extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeStep: 0
+    };
+  }
+
+  componentDidMount() {
+    interval = setInterval(function() {
+      const activeStep = (this.state.activeStep + 1) % numSteps;
+      this.setState({ activeStep });
+    }.bind(this), 2000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(interval);
+  }
+
   render() {
     return (
       <div id='section-process'>
@@ -13,15 +35,24 @@ export default class ProcessSection extends React.Component {
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy <strong>text ever since the 1500s,</strong> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five.</p>
           <div className='row' id='section-process-list'>
             <div className='one-third column'>
-              <div className='icon' id='section-process-feature1'></div>
+              <div className={this.state.activeStep === 0 ? 'active' : ''} id='section-process-feature1'>
+                <img src={require('../../assets/images/biggear.svg')}/>
+                <img src={require('../../assets/images/smallgear.svg')}/>
+              </div>
               <span>Easy Capturing Process</span>
             </div>
             <div className='one-third column'>
-              <div className='icon' id='section-process-feature2'></div>
+              <div className={this.state.activeStep === 1 ? 'active' : ''} id='section-process-feature2'>
+                <img src={require('../../assets/images/biggear.svg')}/>
+                <img src={require('../../assets/images/smallgear.svg')}/>
+              </div>
               <span>Full 360° In Real 3D</span>
             </div>
             <div className='one-third column'>
-              <div className='icon' id='section-process-feature3'></div>
+              <div className={this.state.activeStep === 2 ? 'active' : ''} id='section-process-feature3'>
+                <img src={require('../../assets/images/biggear.svg')}/>
+                <img src={require('../../assets/images/smallgear.svg')}/>
+              </div>
               <span>View With Any VR Glasses</span>
             </div>
           </div>
