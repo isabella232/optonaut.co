5ef9c63b18a75aad6c51f9e36bf221abc30050e7
standfest <standfest@users.noreply.github.com>
2015-04-12 18:21:36 +0200

    Update index.jsx

--- app/components/HardwareSection/index.jsx
+++ app/components/HardwareSection/index.jsx
@@ -28,7 +28,7 @@ export default class HardwareSection extends React.Component {
           <p>To create and view a 3D panorama or an Optograph as we call it, you currently* need three things: an <strong>Optoboard on which you mount two smartphones</strong> (ask your friend to lend you one for a minute) and one of the <Link to='/'>supported VR glasses</Link>.
 </p>
           <a className='button' href="#"><span className='icon'></span>Pre-Order Earlybird Set (12$)</a>
-          <div id='section-hardware-explanation'>* We know it sucks that you need two smartphones to create an Optograph but we got you covered. Our hardware partner Kula3D provides <Link to='/'>a camera-lens which works with one smartphone</Link>.</div>
+          <div id='section-hardware-explanation'>* This solution is the most affordable we could think of. But if you are willing to spend the extra dollar, we provide you with a <Link to='/'>3D lense from our partner Kula.</Link>.</div>
         </div>
       </div>
     );
