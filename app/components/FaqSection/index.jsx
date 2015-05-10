9973b77273545218e2317683a2b6cbc0c28dd104
U-Alpenpunk_III\Emi <emi@eex-dev.net>
2015-05-10 22:10:55 +0200

    Updated Q/A.

--- app/components/FaqSection/index.jsx
+++ app/components/FaqSection/index.jsx
@@ -12,26 +12,33 @@ export default class FaqSection extends React.Component {
           <h2>We Have Your Answers</h2>
           <div className='row'>
             <div className='one-half column'>
-              <strong>Why shoud I use Optonaut, I already have Instagram</strong><br/> In a few weeks, you will be able to order it on this website, to be first in line make sure to subscribe to our newsletter.
+              <strong>What makes Optonaut different from other panorama/photosphere Apps?</strong><br/> 
+              Optonaut records a scene in a stereographic way. That means you can view it in 3D, as if you were there. 
+              You can not only look around, but also perceive depth and dimension. 
             </div>
             <div className='one-half column'>
-              <strong>So what's the difference to panorama images?</strong><br/>Because we work with two different photo spheres, one for each eye, we are able to provide you with a depth experience standard 2D pictures simply cannot provide.
-            </div>
-          </div>
-          <div className='row'>
-            <div className='one-half column'>
-              <strong>Why do I need extra hardware to use Optonaut?</strong><br/> So far we have tested it successfully on all IPhones newer than 4S and all Nexus newer than 4.
-            </div>
-            <div className='one-half column'>
-              <strong>Where can I get the needed hardware?</strong><br/>First large scale Beta tests will be conducted early next year.
+              <strong>Does Optonaut work with my smartphone?</strong><br/>
+              Optonaut will work with most popular Android and iOS devices. 
+              The minimum requirements are having a Camera and Orientation Sensors. 
             </div>
           </div>
           <div className='row'>
             <div className='one-half column'>
-              <strong>When will Optonaut be publicly available?</strong><br/>Inspired by Google Cardboard, we try to make the whole package as affordable as possible. Therefore, we are optimizing all the time and are in lots of negotiations.
+              <strong>When will Optonaut be publicly available?</strong><br/> 
+              Optonaut for Android will be released by the end of August. 
+              The iOS version will be released two months later.
             </div>
             <div className='one-half column'>
-              <strong>Does Optonaut work with my smartphone?</strong><br/> Just like with your eyes you, need two lenses in order to see from two slightly different perspectives. This is necessary to achieve a depth feeling similar to real life.
+              <strong>What is Optonaut going to cost?</strong><br/>
+              Optonaut will be free. We sell your data instead. <br /><br />Just kidding. ;)
+            </div>
+          </div>
+          <div className='row'>
+            <div className='one-half column'>
+              <strong>What data is transmitted to your servers when stitching? </strong><br/>
+              The images including orientation data of your phone. 
+              At the moment we need to transmit those data since the 3D conversion process is very memory intense. 
+              If we find a way around that we’ll let you know. 
             </div>
           </div>
         </div>
