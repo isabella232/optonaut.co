10e4f8265ef13569ea58e054d7259ff7fffc3899
U-Alpenpunk_III\Emi <emi@eex-dev.net>
2015-05-10 22:57:50 +0200

    Fixed some q&a typos.

--- app/components/FaqSection/index.jsx
+++ app/components/FaqSection/index.jsx
@@ -19,25 +19,25 @@ export default class FaqSection extends React.Component {
             <div className='one-half column'>
               <strong>Does Optonaut work with my smartphone?</strong><br/>
               Optonaut will work with most popular Android and iOS devices. 
-              The minimum requirements are having a Camera and Orientation Sensors. 
+              The minimum requirements are having a camera and orientation sensors. 
             </div>
           </div>
           <div className='row'>
             <div className='one-half column'>
               <strong>When will Optonaut be publicly available?</strong><br/> 
-              Optonaut for Android will be released by the end of August. 
+              Optonaut for Android will be released in August 2015. 
               The iOS version will be released two months later.
             </div>
             <div className='one-half column'>
               <strong>What is Optonaut going to cost?</strong><br/>
-              Optonaut will be free. We sell your data instead. <br /><br />Just kidding. ;)
+              Optonaut will be free. We sell your data instead. <br /><br />Just kidding. :)
             </div>
           </div>
           <div className='row'>
             <div className='one-half column'>
               <strong>What data is transmitted to your servers when stitching? </strong><br/>
               The images including orientation data of your phone. 
-              At the moment we need to transmit those data since the 3D conversion process is very memory intense. 
+              At the moment we need to transmit those data since the 3D conversion process is very battery consuming. 
               If we find a way around that we’ll let you know. 
             </div>
           </div>
