ade92c7e49fc0ae57a9d484e6de174884b9e0e21
U-Alpenpunk_III\Emi <emi@eex-dev.net>
2015-05-11 16:10:41 +0200

    Added imprint/legal.

--- app/components/legal/ImprintHandler/index.jsx
+++ app/components/legal/ImprintHandler/index.jsx
@@ -13,10 +13,28 @@ export default class ImprintHandler extends React.Component {
   render() {
     return (
       <div>
-        <br/>
-        <br/>
-        <br/>
-        Someone has to write our legal stuff...
+        Optonaut Johannes Schickling und Emanuel J&ouml;bstl Gbr<br /><br />
+
+        Registered Office: M&uuml;hlburger Straße 8, 76185 Karlsruhe, Germany<br />
+        Phone: +49 152 2149 50 89<br />
+        E-Mail: <a href='mailto:optonaut.io@gmail.com'>optonaut.io@gmail.com</a><br />
+
+        <h3>Copyright</h3>
+
+        The copyright of the contents of the Optonaut website lie with Optonaut.
+        Further usage, also in parts, requires the approval by Optonaut.
+
+        <h3>Disclaimer</h3>
+
+        Optonaut does not assume any liability for the type or correctness of the material available on the
+        Optonaut website. Optonaut is not liable for use of this material. In case contents of the Optonaut
+        website violate legal regulations, we ask you to inform us immediately.
+        The contents will then be removed as quickly as possible.
+
+        <h3>Data Protection</h3>
+        This website makes use of Google Analytics. Therefore, anonymous usage statistics are collected.
+        Data entered into the website is exclusively used for the indicated purposes
+        and not transmitted to 3rd parties. 
       </div>
     );
   }
