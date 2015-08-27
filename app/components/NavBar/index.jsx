bc527d9c840848460dd28482223c1cbe691ade61
Johannes Schickling <schickling.j@gmail.com>
2015-08-27 21:59:41 +0200

    remove demo

--- app/components/NavBar/index.jsx
+++ app/components/NavBar/index.jsx
@@ -50,7 +50,7 @@ export default class NavBar extends React.Component {
           <div className='icon' id='navbar-logo'><Link to='home' onClick={this._scrollToTop}/></div>
           <div id='navbar-nav'>
             <a className='navbar-nav-default' href="/#section-process">How It Works</a>
-            <a className='navbar-nav-special' href='http://demo.optonaut.co'>Demo</a>
+            {/*<a className='navbar-nav-special' href='http://demo.optonaut.co'>Demo</a>*/}
             {/*<a className='navbar-nav-special' href='https://www.kickstarter.com/projects/optonaut/optonaut-virtual-reality-photography'>I Want This</a>*/}
           </div>
           <div onClick={this._toggleNav.bind(this)} className='show-small icon' id='navbar-menu'></div>
